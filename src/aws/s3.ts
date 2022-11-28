581a439281a60903d2b52cfb10e4c5ecc6b57fe6
F56 <tomas950923@gmail.com>
2022-11-28 18:02:46 -0500

    added vercel env

--- src/aws/s3.ts
+++ src/aws/s3.ts
@@ -4,14 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const s3 = new S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  accessKeyId: process.env.LET_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.LET_AWS_SECRET_ACCESS_KEY,
+  region: process.env.LET_AWS_REGION,
 });
 
 const uploadFile = (path: any, _name: any) => {
   const params = {
-    Bucket: process.env.AWS_BUCKET_NAME as string,
+    Bucket: process.env.LET_AWS_BUCKET_NAME as string,
     Key: _name,
     Body: path,
   };
